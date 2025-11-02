de9016a4de56746d6b3ef7040b5ca230c69f97ab
NavEaze Developer <developer@naveaze.com>
2025-11-02 13:00:06 +0200

    v2

---
+++ dpm-web/src/pages/admin/MapEditor.jsx
@@ -0,0 +1,20 @@
+import React, { Suspense } from 'react'
+import { Layout } from '../../components/Layout'
+import { LoadingSpinner } from '../../components/ui/LoadingSpinner'
+
+const FloorplanEditor = React.lazy(() => import('../../components/FloorplanEditor'));
+
+const MapEditorPage = () => {
+  return (
+    <Layout>
+      <div style={{ padding: 16 }}>
+        <h2 style={{ margin: 0, marginBottom: 12 }}>Interactive Map Editor</h2>
+        <Suspense fallback={<div style={{ padding: 24 }}><LoadingSpinner size="lg"/></div>}>
+          <FloorplanEditor />
+        </Suspense>
+      </div>
+    </Layout>
+  )
+}
+
+export default MapEditorPage
